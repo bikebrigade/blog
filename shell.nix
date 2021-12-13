2e21bc3eba4339285dcbda455f3e42268f624cd1
Jenna Blumenthal <jennaleeb@users.noreply.github.com>
2021-12-13 15:12:41 -0500

    Use buildInputs instead of nativeBuildInputs
    Co-authored-by: Max Veytsman <maxim@ontoillogical.com>

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    nativeBuildInputs = [ pkgs.buildPackages.nodejs-16_x ];
+    buildInputs = [ pkgs.buildPackages.nodejs-16_x nodePackages.npm ];
 }
